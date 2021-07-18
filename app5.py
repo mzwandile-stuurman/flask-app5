74dad15776ba2a989a637d1603c43247ba640bac
mzwandile <stuurmanmzwandile@gmail.com>
2021-07-18 17:39:54 +0200

    final update

--- app5.py
+++ app5.py
@@ -2,9 +2,14 @@ from flask import *
 
 app = Flask(__name__)
 
-@app.route("/sign-up", methods=["GET", "POST"])
+@app.route("/", methods=["GET", "POST"])
 def sign_up():
-    return render_template("table.html")
+    return render_template("shopping_list.html")
+
+@app.route('/show_items', methods=['POST', 'GET'])
+def items():
+    results = request.form
+    return render_template('show_items.html', result=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
